a06a1b8ba27a84a4398a3234ac4ac25d3d5428d1
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2021-03-30 22:48:43 -0300

    Add files via upload

---
+++ home/home.component.ts
@@ -0,0 +1,14 @@
+import * as core from '@angular/core';
+@core.Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements core.OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
