d8e4e97e9d942bf106d4fa77c56a91e27e5c26dd
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2021-04-08 22:56:25 -0300

    Add files via upload

---
+++ home/home/home.component.ts
@@ -0,0 +1,33 @@
+import * as core from '@angular/core';
+@core.Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements core.OnInit {
+  formac = [{}];
+  exper = [{}];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  Enviar(): void {
+    console.log('Enviar Arquivo!');
+  }
+  adicinarfor(): void {
+    this.formac.push({});
+    console.log('Adicionar');
+  }
+  removerfor(): void{
+    this.formac.pop();
+    console.log('remover');
+  }
+  adcionarexper(): void {
+    this.exper.push({});
+    console.log('adicionar');
+  }
+  removerexper(): void {
+    this.exper.pop();
+    console.log('remover');
+  }
+}
