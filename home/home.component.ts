8faff8cbb1874400a2c6e98ecff0d580ea025da5
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2021-04-08 23:03:26 -0300

    campos de formação e experiencia
    feito os campos de formação e experiência

--- home/home.component.ts
+++ home/home.component.ts
@@ -5,11 +5,29 @@ import * as core from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements core.OnInit {
+  formac = [{}];
+  exper = [{}];
   constructor() { }
 
   ngOnInit(): void {
   }
   Enviar(): void {
     console.log('Enviar Arquivo!');
+  }
+  adicinarfor(): void {
+    this.formac.push({});
+    console.log('Adicionar');
+  }
+  removerfor(): void{
+    this.formac.pop();
+    console.log('remover');
+  }
+  adcionarexper(): void {
+    this.exper.push({});
+    console.log('adicionar');
+  }
+  removerexper(): void {
+    this.exper.pop();
+    console.log('remover');
   }
 }
